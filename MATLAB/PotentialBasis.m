c674e80983fc657e2a2235debf4571e8173b415e
Xiang Zhang <siang.zhang@gmail.com>
2017-07-05 12:13:44 +0800

    add parallel version

--- MATLAB/PotentialBasis.m
+++ MATLAB/PotentialBasis.m
@@ -10,10 +10,32 @@ function pb=PotentialBasis(path, xr, yr, zr)
     nop=[numel(x) 1];
     points=[reshape(x,nop) reshape(y,nop) reshape(z,nop)];
     noe=size(cb,2);
-    ve=eye(noe);
-    for k=1:noe
-        [pot,fx,fy,fz]=Potential(triangles,cb*ve(:,k),points);
-        pb(:,:,k)=[pot;fx;fy;fz];
-    end
+    
+    % single machine
+    pb=zeros(4,nop(1),noe);
+    
+    % parallel
+    %part=max([xr(3) yr(3) zr(3)])+1;
+    %points=reshape(points,[part,nop(1)/part,3]);
+    %pb=zeros(4,part,nop(1)/part,noe);
+    %parfor i=1:part
+    
+        ve=eye(noe);
+        %parfor k=1:noe
+        for k=1:noe
+            
+            % single machine
+            [pot,fx,fy,fz]=Potential(triangles,cb*ve(:,k),points);
+            pb(:,:,k)=[pot;fx;fy;fz];
+            
+            % parallel
+            %[pot,fx,fy,fz]=Potential(triangles,cb*ve(:,k),shiftdim(points(i,:,:)));
+            %pb(:,i,:,k)=[pot;fx;fy;fz];
+        end
+    
+    % parallel
+    %end
+    %pb=reshape(pb,[4,nop(1),noe]);
+    
     save(file,'pb');
 end
